e81203f5bb786ae6d6575fbf0fcd082cc4a0aa68
iXtreme07 <mario_weisz@yahoo.de>
2022-02-26 13:46:53 +0100

    Create

---
+++ 002_Tell_me_about_you/src/tellMeAboutYou.java
@@ -0,0 +1,11 @@
+public class tellMeAboutYou {
+    public static void main(String[] args) {
+        String firstName = "Max";
+        String lastName = "Mustermann";
+        char sex = 'M';
+        String birthDate = "01 Jänner 2020";
+        float gradePointAverage = 1.1f;
+        boolean married = false;
+        System.out.println("Ich heiße " + firstName + " " + lastName + ", " + "geboren am " + birthDate + " und mein Geschlecht ist: " + sex + "\nFerheiratet: " + married + " und mein Notendurchschnitt ist:" + gradePointAverage);
+    }
+}
